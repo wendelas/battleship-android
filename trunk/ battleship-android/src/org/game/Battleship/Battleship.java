93baedeeed1b6eeae4d2fc3a0a523f436446ff9d
sidg11@gmail.com <sidg11@gmail.com@2f2c9b8c-967c-b004-ceb8-17803a8e3a2f>
2011-04-07 20:26:02 +0000

    added key listeners to menu class

--- trunk/ battleship-android/src/org/game/Battleship/Battleship.java
+++ trunk/ battleship-android/src/org/game/Battleship/Battleship.java
@@ -2,12 +2,44 @@ package org.game.Battleship;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Battleship extends Activity {
+public class Battleship extends Activity implements OnClickListener {
+	
+	Button buttonPlay, buttonHiscore, buttonQuit;
+	TextView testText;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        buttonPlay = (Button)findViewById(R.id.play);
+        buttonHiscore = (Button)findViewById(R.id.hiscore);
+        buttonQuit = (Button)findViewById(R.id.quit);
+        testText = (TextView)findViewById(R.id.testText);
+        
+        buttonPlay.setOnClickListener(this);
+        buttonHiscore.setOnClickListener(this);
+        buttonQuit.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View src) {
+		switch(src.getId()){
+		case R.id.play:
+			testText.setText("You Pressed Play");
+			break;
+		case R.id.hiscore:
+			testText.setText("You Pressed High Score");
+			break;
+		case R.id.quit:
+			testText.setText("You Pressed Quit");
+			break;
+		}		
+	}
 }
